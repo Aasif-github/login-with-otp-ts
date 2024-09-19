01f98c5c360da35a61276ecdb5c050fd6363ce2f
Aasif-github <aasif.github@gmail.com>
2024-09-19 16:51:22 +0530

    fix inputbox validation

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,6 +11,16 @@ const Login = () => {
 
   };
 
+ 
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    // Only allow numbers, remove non-numeric characters
+    // console.log(input);    
+    const sanitizedInput = input.replace(/[^0-9]/g, '');    
+    setMobileNumber(sanitizedInput);
+  };
+
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
@@ -21,11 +31,12 @@ const Login = () => {
               Enter Mobile Number
             </label>
             <input
-              type="tel"
+              type="text"
               id="mobileNumber"
               name="mobileNumber"
               value={mobileNumber}
-              onChange={(e) => setMobileNumber(e.target.value)}
+              maxLength="10"
+              onChange={(e) => handleInputChange (e)}              
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="Enter your mobile number"
               required
